44f3c507523edf0404f484d39cbdd45b5b8d5fa3
Kuldeep Gautam <kuldeep1071996@gmail.com>
2022-12-21 00:07:22 +0530

    Added button

--- src/components/productList.js
+++ src/components/productList.js
@@ -18,34 +18,46 @@ const ProductList = () => {
   return (
     <>
       <br />
-      <div className="container">
+      <div className="container-fluid">
+        <h3 style={{ textAlign: "right" }}>
+          Item Added:&ensp;
+          <b>
+            <button className="btn btn-sm btn-primary">10</button>
+          </b>
+        </h3>
         <h2>Product Lists</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Description</th>
-              <th>Category</th>
-              <th>Image</th>
-              <th>Rating</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product, key) => (
-              <tr key={key}>
-                <td>{product.id}</td>
-                <td>{product.title}</td>
-                <td>{product.price}</td>
-                <td>{product.description}</td>
-                <td>{product.category}</td>
-                <td>{product.image}</td>
-                <td>{product.rating.rate}</td>
+        <div className="over-flow">
+          <table className="table table-striped table-hover">
+            <thead className="table-primary">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Description</th>
+                <th scope="col">Category</th>
+                <th scope="col">Image</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Add item</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {products.map((product, key) => (
+                <tr key={key}>
+                  <td>{product.id}</td>
+                  <td>{product.title}</td>
+                  <td>{product.price}</td>
+                  <td>{product.description}</td>
+                  <td>{product.category}</td>
+                  <td>{product.image}</td>
+                  <td>{product.rating.rate}</td>
+                  <td>
+                    <button className="btn btn-sm btn-primary">Add</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
